0a16e2c19280917ccaa253961ab6fd69818a9ca8
macfeaster <mauritz.zachrisson@gmail.com>
2015-11-16 13:39:22 -0800

    Initial commit of Parser

--- S2/Parser.cs
+++ S2/Parser.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static S2.Token;
 
 namespace S2
 {
     class Parser
     {
         ListDictionary tokens;
+        int repDepth = 0;
 
         // how2entrypoint
         public Parser(ListDictionary tokens)
@@ -16,7 +18,20 @@ namespace S2
             this.tokens = tokens;
         }
 
-        private void Gateway()
+        private void Gateways()
+        {
+            Token t = tokens.NextToken();
+
+            switch (t.type)
+            {
+                case TokenType.BACK:
+                    HandleBACK();
+                    break;
+
+            }
+        }
+
+        private void HandleBACK()
         {
 
         }
