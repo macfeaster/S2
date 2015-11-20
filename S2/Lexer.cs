6e0a74fffa4ebe641fb0c88885d14b901f9b0b3e
Mauritz <mauritzz@kth.se>
2015-11-20 21:56:24 +0100

    Lexer fix for line numbers, debugging output

--- S2/Lexer.cs
+++ S2/Lexer.cs
@@ -23,23 +23,40 @@ namespace S2
             // Read all input, even empty lines and whitespaces
 			while ((line = Console.ReadLine()) != null)
 			{
-                // Check whether line has a comment
-				int commentIndex = line.IndexOf("%");
-
-                // Chop off lines with trailing comments
-                // Replace comment lines with empty lines
-				if (commentIndex > 0)
-					line = line.Substring(0, commentIndex);
-				else if (commentIndex == 0)
-					line = "";
-
-                // Uppercase and append with a newline character
-				input.Append(line.ToUpper());
+                
+                input.Append(line);
                 input.Append("\n");
 			}
 
 			return input.ToString();
 		}
+
+        public string FilterInput(string input)
+        {
+            string[] lines = input.Split('\n');
+            StringBuilder output = new StringBuilder();
+
+            foreach (string line in lines)
+            {
+                // Check whether line has a comment
+                string filteredLine;
+                int commentIndex = line.IndexOf("%");
+
+                // Chop off lines with trailing comments
+                // Replace comment lines with empty lines
+                if (commentIndex > 0)
+                    filteredLine = line.Substring(0, commentIndex);
+                else if (commentIndex == 0)
+                    filteredLine = "";
+                else
+                    filteredLine = line;
+
+                // Uppercase and append with a newline character
+                output.Append(filteredLine.ToUpper());
+            }
+
+            return output.ToString();
+        }
 
         /// <summary>
         /// Parse an input string into a ListDictionary of Tokens.
@@ -51,13 +68,13 @@ namespace S2
 		public List<Token> Parse(string input)
 		{
             // Set up regex tools
-			var pattern = @"(DOWN|UP|FORW|BACK|LEFT|RIGHT|COLOR|REP|[0-9]+|[.]|""|[#][0-9A-F]{6}|\s+|\n)";
+			var pattern = @"(DOWN|UP|FORW|BACK|LEFT|RIGHT|COLOR|REP|[0-9]+|[.]|""|[#][0-9A-F]{6}|(\r|\n|\r\n)|\s+)";
 			var r = new Regex(pattern);
 			var matches = r.Matches(input);
 
             // Parsed tokens are placed in a list, lineCount keeps track of which line errors occur on
             tokens = new List<Token>();
-			int lineNum = 0;
+			int lineNum = 1;
             int lexPos = 0;
 
 			foreach (Match m in matches)
@@ -66,36 +83,47 @@ namespace S2
                 switch (m.Value)
 				{
 					case "DOWN":
+                        Console.WriteLine("Recognized DOWN token on line " + lineNum);
 						tokens.Add(new Token(lineNum, TokenType.DOWN));
 						break;
 					case "UP":
-						tokens.Add(new Token(lineNum, TokenType.UP));
+                        Console.WriteLine("Recognized UP token on line " + lineNum);
+                        tokens.Add(new Token(lineNum, TokenType.UP));
                         break;
                     case "FORW":
                         tokens.Add(new Token(lineNum, TokenType.FORW));
+                        Console.WriteLine("Recognized FORW token on line " + lineNum);
                         break;
                     case "BACK":
                         tokens.Add(new Token(lineNum, TokenType.BACK));
+                        Console.WriteLine("Recognized BACK token on line " + lineNum);
                         break;
                     case "LEFT":
                         tokens.Add(new Token(lineNum, TokenType.LEFT));
+                        Console.WriteLine("Recognized LEFT token on line " + lineNum);
                         break;
                     case "RIGHT":
                         tokens.Add(new Token(lineNum, TokenType.RIGHT));
+                        Console.WriteLine("Recognized RIGHT token on line " + lineNum);
                         break;
                     case "COLOR":
                         tokens.Add(new Token(lineNum, TokenType.COLOR));
+                        Console.WriteLine("Recognized COLOR token on line " + lineNum);
                         break;
                     case "REP":
                         tokens.Add(new Token(lineNum, TokenType.REP));
+                        Console.WriteLine("Recognized REP token on line " + lineNum);
                         break;
                     case ".":
                         tokens.Add(new Token(lineNum, TokenType.DOT));
+                        Console.WriteLine("Recognized DOT token on line " + lineNum);
                         break;
                     case @"""":
                         tokens.Add(new Token(lineNum, TokenType.QUOTE));
+                        Console.WriteLine("Recognized QUOTE token on line " + lineNum);
                         break;
                     case @"\n":
+                        Console.WriteLine("Recognized NEWLINE token on line " + (lineNum + 1));
                         lineNum++;
                         break;
 
@@ -105,27 +133,45 @@ namespace S2
                         // If numeric, add a NUMBER token
                         int val;
                         if (int.TryParse(m.Value, out val))
+                        {
+                            Console.WriteLine("Recognized NUMBER token on line " + lineNum);
                             tokens.Add(new Token(lineNum, TokenType.NUMBER, val));
+                        }
                         // A seven character string, starting with #, is a hex color code
                         // match of our hex regex pattern
                         else if (m.Value.StartsWith("#") && m.Value.Length == 7)
+                        {
+                            Console.WriteLine("Recognized HEX token on line " + lineNum);
                             tokens.Add(new Token(lineNum, TokenType.HEX, m.Value));
+                        }
                         // A matched value which is null or whitespace and is not null
                         // is whitespace
                         else if (string.IsNullOrWhiteSpace(m.Value) && m.Value != null)
-                            tokens.Add(new Token(lineNum, TokenType.WHITESPACE));
+                        {
+                            if (m.Value.IndexOf('\n') >= 0 || m.Value.IndexOf('\r') >= 0)
+                            {
+                                Console.WriteLine("Recognized NEWLINE token on line " + (lineNum + 1));
+                                lineNum++;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Recognized WHITESPACE token on line " + lineNum);
+                                tokens.Add(new Token(lineNum, TokenType.WHITESPACE));
+                            }
+                        }
                         // The matcher has encountered unknown data
                         else
-                            throw new SyntaxError(lineNum);
+                            throw new SyntaxError(lineNum, "Encountered unknown data: " + m.Value);
                         break;
 				}
 
-                lexPos = m.Index;
+                lexPos = m.Index + m.Value.Length;
 			}
 
             // If there is still unparsed data, there is something illegal that cannot be lexed
             if (lexPos != input.Length)
-                throw new SyntaxError(lineNum);
+                throw new SyntaxError(lineNum,
+                    "Remaining illegal tokens: " + input.Substring(lexPos));
 
             return tokens;
 		}
