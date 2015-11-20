1fe12f3d93146fda7dcaacca2a823b63423f7994
Mauritz <mauritzz@kth.se>
2015-11-20 21:56:57 +0100

    Test program implemented, all syntax errors must now report detailed error

--- S2/Program.cs
+++ S2/Program.cs
@@ -10,7 +10,30 @@ namespace S2
 	{
 		static void Main(string[] args)
 		{
+            string program = @"% Det här är en kommentar" + Environment.NewLine +
+                                "% Nu ritar vi en kvadrat" + Environment.NewLine +
+                                "DOWN." + Environment.NewLine +
+                                "FORW 1. LEFT 90." + Environment.NewLine +
+                                "FORW 1. LEFT 90." + Environment.NewLine +
+                                "FORW 1. LEFT 90." + Environment.NewLine;
 
-		}
+            Console.WriteLine("FORW 1. LEFT 90.\n".IndexOf('\n'));
+
+            /* try
+            {*/
+                Lexer l = new Lexer();
+                string preprocessed = l.FilterInput(program);
+                List<Token> parsed = l.Parse(preprocessed);
+
+                Parser p = new Parser(parsed);
+                List<Instruction> tree = p.GetTree();
+            /* }
+            catch (SyntaxError e)
+            {
+                Console.WriteLine(e.Message);
+            } */
+
+            Console.ReadLine();
+        }
 	}
 }
--- S2/SyntaxError.cs
+++ S2/SyntaxError.cs
@@ -5,7 +5,7 @@ namespace S2
     [Serializable]
     internal class SyntaxError : Exception
     {
-        public SyntaxError(int line) : base("Syntaxfel på rad " + line) {}
+        // public SyntaxError(int line) : base("Syntaxfel på rad " + line) {}
 
         public SyntaxError(int line, string message) : base("Syntaxfel på rad " + line + ": " + message) {}
 
