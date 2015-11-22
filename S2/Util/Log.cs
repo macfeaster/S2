b2f4e03c8c1ee4c12d009a25fb793fd8ee95f1f9
Mauritz <mauritzz@kth.se>
2015-11-22 15:31:54 +0100

    Commented Log.cs

--- S2/Util/Log.cs
+++ S2/Util/Log.cs
@@ -1,7 +1,14 @@
-﻿using System;
+﻿// Log.cs
+// Part of the KTH course DD1361 Programming Paradigms lab S2
+// Authors: Alice Heavey and Mauritz Zachrisson
+
+using System;
 
 namespace S2
 {
+    /// <summary>
+    /// Logs program internals to stdout when DEBUG constant is defined.
+    /// </summary>
     internal static class Log
     {
         public static void Debug(string message)
